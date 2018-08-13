0843c9227dc795773a634012301a5df6414fd634
NOORABDELHALIM <nur.abdelhalim@rebels.ridgenet.org>
2018-08-13 14:04:14 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -48,11 +48,8 @@ class GardenGnome
       "GNARLY!!!"
     end 
   
-    def initialize(name,age)
-      @name = name 
-      @age = age 
-      puts "Hello humans, my name is #{@name}, I am #{@age} years old, and you'll rue the day you crossed me!"
+    def introduce_self
+      return "Hello humans, my name is #{name}, I am #{age} years old, and you'll rue the day you crossed me!"
     end
   end 
-    
-carl = Introduction.new("Carl","24")
+  
