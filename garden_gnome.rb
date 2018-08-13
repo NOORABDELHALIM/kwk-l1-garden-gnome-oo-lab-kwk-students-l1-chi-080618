7307049dac5e2507aab34579c72491b6d2ec88f1
NOORABDELHALIM <nur.abdelhalim@rebels.ridgenet.org>
2018-08-13 14:02:43 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -47,11 +47,11 @@ class GardenGnome
     def shout 
       "GNARLY!!!"
     end 
-    # def initialize(name,age)
-    #   @name = name 
-    #   @age = age 
-    #   puts "Hello humans, my name is #{@name}, I am #{@age} years old, and you'll rue the day you crossed me!"
-    # end 
+    def initialize(name,age)
+      @name = name 
+      @age = age 
+      puts "Hello humans, my name is #{@name}, I am #{@age} years old, and you'll rue the day you crossed me!"
+    end 
       
   end 
   
